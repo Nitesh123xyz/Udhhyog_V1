674d0547d0a149fe3d11d72d7e6d8823ffb1a775
Nitesh <nitesh8825kr@gmail.com>
2025-08-31 13:14:13 +0530

    change in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -83,7 +83,7 @@ const Login = () => {
           {/* Header */}
           <div className="text-center mb-6 sm:mb-6">
             <h1 className="text-gray-100 text-[1.3rem] sm:text-[2rem] font-bold">
-              UDHHYOG CRM V1
+              UDHHYOG CRM V1 Nitesh
             </h1>
             <p className="text-gray-100 text-[0.8rem] sm:text-base mt-2">
               Log in to your account
